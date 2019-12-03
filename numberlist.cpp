4672bb1936eeedaf7c164e6ec68b75f3f871c502
Ubuntu <ubuntu@ip-172-31-27-73.ec2.internal>
2019-12-03 02:06:27 +0000

    Initial commit

---
+++ numberlist.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2019 Jacob Bonner All rights reserved.
+//
+// Created by: Jacob Bonner
+// Created on: December 2019
+// This program adds 10 numbers to an array then calculates the average
+
+#include <iostream>
+#include <ctime>
+
+int main() {
+    // This function adds 10 numbers to an array then calculates the average
+
+    // Random number seed
+    unsigned int seed = time(NULL);
+
+    // Variables and arrays
+    int randomNumber;
+    int sum = 0;
+    float average;
+    int numberArray[10];
+
+    // Process
+    for (int counter = 0; counter < 10; counter++) {
+        randomNumber = rand_r(&seed) % 100;
+        numberArray[counter] = randomNumber;
+        sum = sum + numberArray[counter];
+    }
+
+    average = sum/10.0;
+
+    // Output
+    std::cout << "The average of your numbers is " << average << std::endl;
+}
